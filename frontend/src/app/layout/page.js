2fbb0a1a1bd9ee6a4def5f2a7085c82fbc771e0a
shrikant-d5456 <157231725+shrikant-d5456@users.noreply.github.com>
2024-04-29 11:11:33 +0530

    Update page.js

--- frontend/src/app/layout/page.js
+++ frontend/src/app/layout/page.js
@@ -5,7 +5,7 @@ import Footer from "../components/common/Footer";
 
 const Home = () => {
   return (
-    <main className="w-screen min-h-screen flex flex-col items-center">
+    <main className="w-full min-h-screen flex flex-col items-center">
       <HeroSection />
       <Navbar />
       <Middle/>

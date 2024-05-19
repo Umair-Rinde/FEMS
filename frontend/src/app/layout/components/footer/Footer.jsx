76db424461d73b4fa6b04ab7fd02261c1a705ae2
Shubham Kharade <shubham.kharade28@gmail.com>
2024-05-19 21:40:49 +0530

    "nothing"

---
+++ frontend/src/app/layout/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+    <footer className='text-white text-center bg-black '>
+    footer data
+  </footer>
+  </main>
+  )
+}

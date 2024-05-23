ebee8242e87b5da41129b730617acf943e6227bb
Shubham Kharade <shubham.kharade28@gmail.com>
2024-05-23 13:59:36 +0530

    shubham: Initiated root folder as npm project, and added commands 'npm run server' and 'npm run client'

--- frontend/src/app/layout/page.js
+++ frontend/src/app/layout/page.js
@@ -10,7 +10,6 @@ const Home = () => {
       <Navbar />
       <Middle/>
       <Footer/>
-      
     </main>
   )
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,15 @@
+import HeroSection from "@/app/components/common/HeroSection";
+import Navbar from "@/app/components/common/Navbar";
+import Middle from "@/app/components/common/Middle";
+import Footer from "@/app/components/common/Footer";
 
 const Home = () => {
   return (
-    <main className="w-screen min-h-screen">
-      
+    <main className="w-screen min-h-screen flex flex-col items-center scroll-hidden">
+      <HeroSection />
+      <Navbar />
+      <Middle/>
+      <Footer/>
     </main>
   )
 }

d13dc4dd3fef1f62ec5fd3021f3593d508cfec82
Shubham Kharade <shubham.kharade28@gmail.com>
2024-10-12 15:17:12 +0530

    css error solved

--- frontend/src/app/hoc/PageWrapper.jsx
+++ frontend/src/app/hoc/PageWrapper.jsx
@@ -4,10 +4,10 @@ import { Navbar, Footer, Header } from "../components";
 const PageWraper = (PageComponent, id = null) =>
   function Page() {
     return (
-      <div className="w-full min-h-screen flex flex-col">
+      <div className="w-full min-h-screen flex flex-col bg-white">
         <Header />
         <Navbar />
-        <div className="flex items-center justify-center w-full py-10 px-4 md:px-20">
+        <div className="flex items-center justify-center min-h-screen w-full py-10 px-4 md:px-20">
           <PageComponent />
         </div>
         <Footer />

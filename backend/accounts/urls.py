fd8fa8984a2f457d4f7a3673db024438f9aa100f
Umair-Rinde <rindeumair@gmail.com>
2025-01-13 16:37:26 +0530

    removed unwanted code

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,7 +6,6 @@ from crop.views import FarmerAPIView
 urlpatterns = [
     path('login/', LoginApiView.as_view()),
     path('register/',UserRegistrationAPI.as_view(), name='user-register'),
-    path('verify-otp/',UserOtpVerificationAPI.as_view()),
     path('farmers/', FarmerAPIView.as_view(), name='farmer-list'),
     path('farmers/<uuid:id>/', FarmerAPIView.as_view(), name='farmer-detail'),
 ]
